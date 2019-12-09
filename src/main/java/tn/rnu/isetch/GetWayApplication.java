6b7edc4f295037bee01a3f9c32bf5a8e07803962
kinbachir <bachir422018@gmail.com>
2019-12-09 21:40:39 +0100

    Initial commit

---
+++ src/main/java/tn/rnu/isetch/GetWayApplication.java
@@ -0,0 +1,13 @@
+package tn.rnu.isetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetWayApplication.class, args);
+	}
+
+}
